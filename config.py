a6d0ce65fe819606f91540e986ee6bca678ad467
Leone222 <88062373+Leone222@users.noreply.github.com>
2024-06-25 17:14:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-TOKEN = '7411351827:AAGVi3E9WOUiQEpfiRORcxiyiCDunNNTZv4' # Токен бота, берём из BotFather
+TOKEN = 'TOKEN' # Токен бота, берём из BotFather
 
-chat_id = -4281298887 ## ID группового чата где будет работать бот, узнать его можно в боте @LeadConverterToolkitBot
+chat_id = 0 ## ID группового чата где будет работать бот, узнать его можно в боте @LeadConverterToolkitBot
 
-api = "b648bb2fc9msh9f4deb3e6331688p14c473jsnb5bbb71c96b2" # Tik Tok API key - берём из https://rapidapi.com/yi005/api/tiktok-video-no-watermark2/playground/apiendpoint_0350524f-f088-42c9-abdb-2d1445094acc
+api = "api" # Tik Tok API key - берём из https://rapidapi.com/yi005/api/tiktok-video-no-watermark2/playground/apiendpoint_0350524f-f088-42c9-abdb-2d1445094acc
 
 admin_us = 'admin' # Юз админа, используется в некоторых сообщениях для связи с админом чтобы купить рекламу/спонсорство
