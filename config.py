31475febddc1f44e89c8434d89426cde0e1b2b05
Leone222 <88062373+Leone222@users.noreply.github.com>
2024-06-26 11:57:29 +0300

    Update config.py
    Добавил инструкцию для получения API tik tok

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ TOKEN = 'TOKEN' # Токен бота, берём из BotFather
 
 chat_id = 0 ## ID группового чата где будет работать бот, узнать его можно в боте @LeadConverterToolkitBot
 
-api = "api" # Tik Tok API key - берём из https://rapidapi.com/yi005/api/tiktok-video-no-watermark2/playground/apiendpoint_0350524f-f088-42c9-abdb-2d1445094acc
+api = "api" # Tik Tok API key - берём из https://rapidapi.com/yi005/api/tiktok-video-no-watermark2/playground/apiendpoint_0350524f-f088-42c9-abdb-2d1445094acc , инструкция для получения API - https://telegra.ph/API-dlya-TikTok-06-26
 
 admin_us = 'admin' # Юз админа, используется в некоторых сообщениях для связи с админом чтобы купить рекламу/спонсорство
+
+# Если у кого-нибудь проблемы со скриптом или есть идеи для улучшения, отпишите в телеграмм создателю @mainreal0
